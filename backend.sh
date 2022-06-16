c150de40d3924a562ab751fcb2bcd328bc59303c
johnmogi <anguru@gmail.com>
2022-06-17 01:13:00 +0300

    up

--- backend.sh
+++ backend.sh
@@ -9,7 +9,7 @@ apt-get install \
  curl -fsSL https://download.docker.com/linux/ubuntu/gpg -y | gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null -y
+  $(lsb_release -cs) stable" -y | tee /etc/apt/sources.list.d/docker.list > /dev/null -y
   apt update -y
   apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
   docker pull postgres:latest -y
