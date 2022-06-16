33d2746fbda3182cc2a2c71c067667e20f889437
johnmogi <anguru@gmail.com>
2022-06-17 01:11:11 +0300

    up

--- backend.sh
+++ backend.sh
@@ -5,8 +5,8 @@ apt-get install \
     curl \
     gnupg \
     lsb-release
-    mkdir -p /etc/apt/keyrings
- curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg 
+    mkdir -p /etc/apt/keyrings -y
+ curl -fsSL https://download.docker.com/linux/ubuntu/gpg -y | gpg --dearmor -o /etc/apt/keyrings/docker.gpg -y
  echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null -y
